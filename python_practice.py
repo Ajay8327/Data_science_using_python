24b7712e6c472093a4a4c0a2b2cf36844ae4c276
Ajay8327 <102033157+Ajay8327@users.noreply.github.com>
2022-04-08 13:14:55 +0530

    Add files via upload

---
+++ python_practice.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd 
+
+
+# In[4]:
+
+
+df=pd.read_csv('C:\\Users\\GURU\\Downloads\\salaries.csv')
+
+
+# In[10]:
+
+
+df.head()
+
+
+# In[13]:
+
+
+df.tail(10)
+
+
+# In[15]:
+
+
+df.shape
+
+
+# In[16]:
+
+
+df.describe
+
+
+# In[17]:
+
+
+df.dtypes
+
+
+# In[18]:
+
+
+df.axes
+
+
+# In[19]:
+
+
+df.values
+
+
+# In[20]:
+
+
+df.ndim
+
+
+# In[21]:
+
+
+df.describe()
+
+
+# In[25]:
+
+
+df.std()
+
+
+# In[26]:
+
+
+dff=df.head(50)
+
+
+# In[28]:
+
+
+dff.std()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
